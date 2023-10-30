751a77291d209cf123940b7a5140a92f5abf6559
sabiransari1 <ansariisabir1@gmail.com>
2023-10-30 18:45:09 +0530

    masai-hospital-server

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const connection = require('./db.js');
+const userRoutes = require('./routes/userRoutes.js');
+
+app.use(express.json());
+app.use(express.text());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  try {
+    return res.status(200).send('Masai Hosptail ...');
+  } catch (error) {
+    return res.status(400).send({ error: error.message });
+  }
+});
+
+app.use('/user', userRoutes);
+
+app.listen(process.env.PORT || 7070, () => {
+  try {
+    connection();
+
+    console.log(`Server is running on port ${process.env.PORT || 7070}...`);
+  } catch (error) {
+    console.log({ error: error.message });
+  }
+});
---
+++ routes/userRoutes.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const userRoutes = express.Router();
+const userModel = require('../models/userModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+userRoutes.post('/signup', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const userCheck = await userModel.findOne({ email });
+
+    if (userCheck) {
+      return res.status(400).send({ msg: 'User Already Signup' });
+    }
+
+    const hashPassword = await bcrypt.hash(password, 8);
+
+    const user = await userModel.create({
+      ...req.body,
+      password: hashPassword,
+    });
+
+    return res.status(200).send({ msg: 'User Signup Successfull', user });
+  } catch (error) {
+    return res.status(400).send({ error: error.message });
+  }
+});
+
+userRoutes.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const userCheck = await userModel.findOne({ email });
+
+    const passwordCheck = await bcrypt.compare(password, userCheck.password);
+
+    if (passwordCheck) {
+      const token = jwt.sign({ userId: userCheck.id }, process.env.SECRET_KEY);
+
+      return res.status(200).send({ msg: 'User Signup Successfull', token });
+    } else {
+      return res.status(400).send({ msg: 'Incorrect Password' });
+    }
+  } catch (error) {
+    return res.status(400).send({ error: error.message });
+  }
+});
+
+module.exports = userRoutes;
